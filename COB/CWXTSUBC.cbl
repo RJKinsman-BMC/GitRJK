43717049e8ca0065604a0e42a78aa6a385c4ef3e
roland_kinsman <roland_kinsman@bmc.com>
2023-03-07 09:55:18 -0500

    ebcdic characters in value clause

--- COB/CWXTSUBC.cbl
+++ COB/CWXTSUBC.cbl
@@ -28,6 +28,8 @@
       ***
       ***
       ***
+       77  EBCDIC-STRING PIC X(44) VALUE 
+           IS 'ꞸꞹꞺꞻꞼꞽꞾꞿꟀꟁꟂꟃꟄ*»«ꟈꟉꟊꟋꟌꟍ꟎꟏Ꟑꟑ꟒ꟓ꟔ꟕꟖꟗꟘꟙꟚꟛꟜ*꟞꟟꟠꟡꟢꟣꟤꟥꟦꟧ '.
        01  MGMT-RANGE        PIC 9(6)V99.
            88  M-RANGE1           VALUES ARE      1 THRU 100000.
            88  M-RANGE2           VALUES ARE 100001 THRU 200000.
